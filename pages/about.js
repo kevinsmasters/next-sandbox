ca9c05af138974db3fd57fc983bd3d4c437002ac
Kevin Masters <kevin.masters@taconic.com>
2021-02-05 09:53:03 -0500

    added about template

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+        <Head>
+          <title>About WebDev Foolz</title>
+          <meta name='keywords' content='abouts, web dev, programming, tacos' />
+        </Head>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default about
